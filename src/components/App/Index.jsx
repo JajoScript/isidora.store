7a41821e9cfbe8c3ae6d1e639ba25977b1f6b482
Javier Almarza <javieralmarzabucarey@gmail.com>
2023-01-20 17:35:41 -0400

    nuevo compilador

---
+++ src/components/App/Index.jsx
@@ -0,0 +1,21 @@
+// Dependencias.
+import React from "react";
+
+// Definición del componente: <App />
+function App(props) {
+  // 1. Manejo del estado.
+  // 2. Ciclo de vida.
+  // 3. Metodos.
+  // 4. Render.
+  return (
+    <React.Fragment>
+      <h1>Hola Isi</h1>
+    </React.Fragment>
+  );
+}
+
+// Proptypes.
+App.propTypes = {};
+
+// Exportación.
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+// Dependencias.
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+
+// Estilos.
+
+// Renderizado.
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
